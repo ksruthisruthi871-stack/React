f8dd079997f70c7ec74d61b3a08e388038684ee7
ksruthisruthi871-stack <ksruthisruthi871@gmail.com>
2026-06-18 15:25:53 +0530

    Form

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import JSX from "./events.jsx"
+import JSX from "./application.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
